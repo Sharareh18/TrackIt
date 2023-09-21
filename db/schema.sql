e22f2d8e57b87734d429045164836d9dc5cb5dc2
Sharareh Golshani <shararehg79@yahoo.com>
2023-09-20 20:17:15 -0700

    created initial db files

---
+++ db/schema.sql
@@ -0,0 +1,27 @@
+DROP DATABASE IF EXISTS businessDB;
+CREATE database businessDB;
+
+USE businessDB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,   
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id));
+
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL 
+    -- UNSIGNED DEFAULT 1.00,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id),
+    PRIMARY KEY (id));
+
+CREATE TABLE employee (
+    id int not null auto_increment,
+    first_name varchar(30)not null,
+    last_name varchar(30)not null,
+    role_id int references role(id),
+    manager_id int references manager(id),
+    PRIMARY key (id));
+   
